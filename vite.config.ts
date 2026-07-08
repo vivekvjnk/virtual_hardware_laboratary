7cae478eec242cee5e31bcab425856aabc9b38a6
Archy Agent <archy@vhl.ai>
2026-07-08 22:30:32 +0530

    refactor(update packages)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      ignored: ['**/node_modules/**', '**/dist/**'],
+    },
+  },
+  // This completely mutes the source map warnings from flooding the terminal
+  logLevel: 'info',
 })
